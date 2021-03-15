c1cfb626b162edf26ba35d7aa33f9c41baf11f64
Ufuk Özsoy <40505074@live.napier.ac.uk>
2021-03-15 21:22:29 +0100

    unit tests

--- src/test/java/com/napier/sem/CityTest.java
+++ src/test/java/com/napier/sem/CityTest.java
@@ -8,69 +8,65 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * This Class Tests for the City Data Model to verify that the correct data is
- * stored in the structs correctly.
- */
 
 class CityTest {
     @Test
     void getId() {
-        //Arrange
-        CityReportRowMapper city = new CityReportRowMapper(1,"Varna", "169", "Varna", 100000);
 
-        //Assert
+        CityReportRowMapper city = new CityReportRowMapper(1,"Kabul", "ABW", "Kabol", 1780000);
+
+
         assertEquals(1, city.getId());
     }
 
     @Test
     void getName() {
-        //Arrange
-        CityReportRowMapper city = new CityReportRowMapper(1,"Varna", "169", "Varna", 100000);
 
-        //Assert
-        assertEquals("Varna", city.getName());
+        CityReportRowMapper city = new CityReportRowMapper(1,"Kabul", "ABW", "Kabol", 1780000);
+
+
+        assertEquals("Kabul", city.getName());
     }
 
     @Test
     void getPopulation() {
-        //Arrange
-        CityReportRowMapper city = new CityReportRowMapper(1,"Varna", "169", "Varna", 100000);
 
-        //Assert
-        assertEquals(100000, city.getPopulation());
+        CityReportRowMapper city = new CityReportRowMapper(1,"Kabul", "ABW", "Kabol", 1780000);
+
+
+        assertEquals(1780000, city.getPopulation());
     }
 
     @Test
     void getCountry_code() {
-        //Arrange
-        CityReportRowMapper city = new CityReportRowMapper(1,"Varna", "169", "Varna", 100000);
 
-        //Assert
-        assertEquals("169", city.getCountry_code());
+        CityReportRowMapper city = new CityReportRowMapper(1,"Kabul", "ABW", "Kabol", 1780000);
+
+
+        assertEquals("ABW", city.getCountry_code());
     }
 
     @Test
     void getDistrict() {
-        //Arrange
-        CityReportRowMapper city = new CityReportRowMapper(1,"Varna", "169", "Varna", 100000);
 
-        //Assert
-        assertEquals("Varna", city.getDistrict());
+        CityReportRowMapper city = new CityReportRowMapper(1,"Kabul", "ABW", "Kabol", 1780000);
+
+
+        assertEquals("Kabol", city.getDistrict());
     }
 
     @Test
     void toStringTest() {
-        //Arrange
-        CityReportRowMapper city = new CityReportRowMapper(1,"Varna", "169", "Varna", 100000);
 
-        //Assert
+        CityReportRowMapper city = new CityReportRowMapper(1,"Kabul", "ABW", "Kabol", 1780000);
+
+
         assertEquals("City{" +
                 "id=1" +
-                ", name='Varna" + '\'' +
-                ", country_code='169" + '\'' +
-                ", district='Varna" + '\'' +
-                ", population=100000" +
+                ", name='Kabul" + '\'' +
+                ", country_code='ABW" + '\'' +
+                ", district='Kabol" + '\'' +
+                ", population=1780000" +
                 '}', city.toString());
     }
 }
