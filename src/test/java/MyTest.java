e3bf53409aa4d4295326bdfd86f26b3e14ec32ea
Ufuk Özsoy <40505074@live.napier.ac.uk>
2021-03-15 20:19:34 +0100

    unit tests

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class MyTest
+{
+    @Test
+    void unitTest1()
+    {
+        assertEquals(5, 5);
+    }
+}
