91023ed2ec593d2b0b5044e8e08c8adc3c7e5430
Ufuk Özsoy <40505074@live.napier.ac.uk>
2021-03-16 10:41:48 +0100

    unit tests

--- src/main/java/com/napier/sem/mappers/reports/CapitalCityReportRowMapper.java
+++ src/main/java/com/napier/sem/mappers/reports/CapitalCityReportRowMapper.java
@@ -31,4 +31,68 @@ public class CapitalCityReportRowMapper {
         }
         return objectList;
     }
+    private final int id;
+    private final String name;
+    private final String country_code;
+    private final String district;
+    private final int population;
+
+    public CapitalCityReportRowMapper(int id, String name, String country_code, String district, int population) {
+        this.id = id;
+        this.name = name;
+        this.country_code = country_code;
+        this.district = district;
+        this.population = population;
+    }
+
+    /**
+     *
+     * @return The unique identifier number of this city
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     *
+     * @return The name of this city
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     *
+     * @return The country code of this city
+     */
+    public String getCountry_code() {
+        return country_code;
+    }
+
+    /**
+     *
+     * @return The district of this city
+     */
+    public String getDistrict() {
+        return district;
+    }
+
+    /**
+     *
+     * @return The population of this city
+     */
+    public int getPopulation() {
+        return population;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "id=" + this.id +
+                ", name='" + this.name + '\'' +
+                ", country_code='" + this.country_code + '\'' +
+                ", district='" + this.district + '\'' +
+                ", population=" + this.population +
+                '}';
+    }
 }
--- src/test/java/com/napier/sem/CapitalCityTest.java
+++ src/test/java/com/napier/sem/CapitalCityTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-class CityTest {
+class CapitalCityTest {
     @Test
     void getId() {
 
